1d6d37634268d215c2ec3ca79f5d6f20d70f43e8
inch <sxin2949@naver.com>
2021-06-21 20:04:35 +0900

    #13.7 AppLoading part Two

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import React, {useState} from 'react';
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
 import { StyleSheet, Text, View } from 'react-native';
+import {Asset} from "expo-asset";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
@@ -11,7 +12,10 @@ export default function App() {
   const preload = () => {
     const fontsToLoad = [Ionicons.font];
     const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
-    return Promise.all(fontPromises);
+    const imagesToLoad = [require("./assets/logo.jpeg"),
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png"];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    return Promise.all([...fontPromises, ...imagePromises]);
   };
   if (loading) {
     return (
