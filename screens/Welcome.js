bfc2e29f7bef686e41a0e69a9a02276d405e551e
inch <sxin2949@naver.com>
2021-06-27 21:27:31 +0900

    colors를 찾아헤맴

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,29 +1,16 @@
 import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
-import AuthButton from "../components/AuthButton";
-
-const Container = styled.View`
-    flex:1;
-    background-color: black;
-    justify-content: center;
-    align-content: center;
-    padding: 0px 40px;
-`;
-
-const Logo = styled.Image`
-    max-width: 20%;margin:0 auto;height:100px;
-`;
-
 
 
 const LoginLink = styled.TouchableOpacity`
     margin:0 auto;margin-top:10px;
 `;
 
-const LoginText = styled.Text`
-    font-size:12px;color:${colors.blue};
-`;
+
+
+import AuthButton from "../components/auth/AuthButton";
+import AuthLayout from "../components/auth/AuthLayout";
 
 export default function Welcome(props) {
 
@@ -31,17 +18,16 @@ export default function Welcome(props) {
     const goToLogIn = () => props.navigation.navigate("LogIn");
 
     return ( 
-        <Container>
-            <Logo resizeMode="center" source={require("../assets/logo.png")} />
-            <AuthButton text="Create New Account " disabled={false} onPress={onPress} /> 
-
-        
-            <LoginLink onPress={goToLogIn}>
-                <LoginText>
-                    Log in
-                </LoginText>
-            </LoginLink>
-        </Container>
+        <AuthLayout>
+            <AuthButton
+                text="Crate New Account"
+                disabled={false}
+                onPress={goToCreateAccount}
+            />
+            <TouchableOpacity onPress={goToLogIn}>
+            <LoginLink>Log In</LoginLink>
+            </TouchableOpacity>
+        </AuthLayout>
     )
 };
 
