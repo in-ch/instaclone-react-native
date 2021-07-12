144fef3961444850d7a6eab83cd5bddc4628dc0f
inch <sxin2949@naver.com>
2021-07-12 23:47:45 +0900

    #15.8 Photo part Three

--- components/Photo.js
+++ components/Photo.js
@@ -4,6 +4,7 @@ import { useNavigation } from "@react-navigation/native";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 import { useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const Container = styled.View`
     border: 1px solid blue;
@@ -24,14 +25,27 @@ const Username = styled.Text`
     font-weight: 600;
 `;
 const File = styled.Image``;
-const Actions = styled.View``;
-const Action = styled.TouchableOpacity``;
-const Caption = styled.View``;
+const Actions = styled.View`
+    flex-direction: row;
+    align-items:center;
+`;
+const Action = styled.TouchableOpacity`
+    margin-right:10px;
+`;
+const Caption = styled.View`
+    flex-direction: row;
+`;
 const CaptionText = styled.Text`
     color:white;
+    margin-left:10px;
 `;
 const Likes = styled.Text`
     color:white;
+    margin: 7px 0px;
+    font-weight: 600;
+`;
+const ExtraContainer = styled.Text`
+    padding: 10px;
 `;
 
 export default function Photo({id, user, caption, file, isLiked, likes}){
@@ -47,12 +61,12 @@ export default function Photo({id, user, caption, file, isLiked, likes}){
 
     return <Container>
             <Header onPress={()=> navigation.navigate("Profile")}>
-                    <UserAvatar 
-                        resizeMode="cover" 
-                        style={{width:30,height:30,borderRadius:25}}
-                        source={{uri: user.avatar}}
-                    />
-                    <Username>{user.userName}</Username>
+                <UserAvatar 
+                    resizeMode="cover" 
+                    style={{width:30,height:30,borderRadius:25}}
+                    source={{uri: user.avatar}}
+                />
+                <Username>{user.userName}</Username>
             </Header>
             <File
                 resizeMode="contain"
@@ -63,16 +77,23 @@ export default function Photo({id, user, caption, file, isLiked, likes}){
                 source={{ uri: file }}
             />
             <Actions>
-                <Action />
-                <Action />
+                <Action>
+                    <Ionicons name={isLiked ? "heart" : "heart-outline"} color={isLiked ? "tomato":"white"} size={22} />
+                </Action>
+                <Action onPress={()=> navigation.navigate("Comments")}>
+                    <Ionicons name="chatbubble-outline" color="white" size={22} />
+                </Action>
             </Actions>
-            <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
+            <TouchableOpacity  onPress={()=> navigation.navigate("Likes")}>
+                <Likes>{likes === 1 ? "1 like" : `${likes} likes`}</Likes>
+            </TouchableOpacity>
             <Caption>
                 <TouchableOpacity onPress={()=> navigation.navigate("Profile")}>
                     <Username>{user.userName}</Username>
                 </TouchableOpacity>
                 <CaptionText>{caption}</CaptionText>
             </Caption>
+            <ExtraContainer />
         </Container>
 }
 
--- components/nav/StackNavFactory.js
+++ components/nav/StackNavFactory.js
@@ -2,6 +2,8 @@ import React from "react";
 import {createStackNavigator} from "@react-navigation/stack";
 
 import Photo from "../../screens/Photo";
+import Likes from "../../screens/Likes";
+import Comments from "../../screens/Comments";
 import Profile from "../../screens/Profile";
 import Feed from "../../screens/Feed";
 import Search from "../../screens/Search";
@@ -38,6 +40,8 @@ export default function StachNavFactory({ screenName }) {
             {screenName === "Me" ? <Stack.Screen name={"Me"} component={Me} /> : null}
             <Stack.Screen name="Profile" component={Profile} />
             <Stack.Screen name="Photo" component={Photo} />
+            <Stack.Screen name="Likes" component={Likes} />
+            <Stack.Screen name="Comments" component={Comments} />
       </Stack.Navigator>
     )
 }
