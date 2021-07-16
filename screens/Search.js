75f4a107eb893f8f19fe84b3cb089c8ef4af0dbb
inch <sxin2949@naver.com>
2021-07-16 22:09:54 +0900

    #16.5 Search part Three

--- screens/Search.js
+++ screens/Search.js
@@ -1,13 +1,22 @@
 import { gql, useLazyQuery } from "@apollo/client";
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
-import { Text, TextInput, View } from "react-native";
+import { ActivityIndicator, Text, TextInput, View } from "react-native";
 import styled from "styled-components/native";
 import DismissKeyboard from "../components/DismissKeyboard";
 
-const Input = styled.TextInput``;
+const MessageContainer = styled.View`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+`;
+const MessageText = styled.Text`
+  margin-top: 15px;
+  color: white;
+  font-weight: 600;
+`;
 
-const SEARCH_PHOTO = gql`
+const SEARCH_PHOTOS = gql`
     query searchPhotos($keyword: String!) {
       searchPhotos(keyword: $keyword) {
         id
@@ -17,9 +26,16 @@ const SEARCH_PHOTO = gql`
 `;
 
 export default function Search({navigation}) {
-  const { setValue, register } = useForm();
-  const [startQueryFn, { loading, data }] = useLazyQuery(SEARCH_PHOTOS);
-  
+  const { setValue, register, watch, handleSubmit } = useForm();
+  const [startQueryFn, { loading, data, called }] = useLazyQuery(SEARCH_PHOTOS);
+  const onValid = ({ keyword }) => {
+    startQueryFn({
+      variables: {
+        keyword,
+      },
+    });
+  };
+
   const SearchBox = () => (
     <TextInput
       style={{ backgroundColor: "white" }}
@@ -30,25 +46,39 @@ export default function Search({navigation}) {
       returnKeyType="search"   // IOS용, 키보드에서 Submit 부분 수정
       autoCorrect={false}   // 자동 완성 기능 빼기
       onChangeText={(text) => setValue("keyword", text)}
+      onSubmitEditing={handleSubmit(onValid)}
     />
   );
   useEffect(() => {
     navigation.setOptions({
       headerTitle: SearchBox,
     });
-    register("keyword");
+    register("keyword", {
+      required: true,
+      minLength: 3,
+    });
   }, []);
 
   return (
     <DismissKeyboard>
-      <View
-        style={{
-          backgroundColor: "black",
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
+      <View style={{ flex: 1, backgroundColor: "black" }}>
+          {loading ? (
+            <MessageContainer>
+              <ActivityIndicator size="large" />
+              <MessageText>Searching...</MessageText>
+            </MessageContainer>
+          ) : null}
+          {!called ? (
+            <MessageContainer>
+              <MessageText>Search by keyword</MessageText>
+            </MessageContainer>
+          ) : null}
+          {data?.searchPhotos !== undefined &&
+          data?.searchPhotos?.length === 0 ? (
+            <MessageContainer>
+              <MessageText>Could not find anything.</MessageText>
+            </MessageContainer>
+          ) : null}
          <Text style={{ color: "white" }}>Photo</Text>
       </View>
     </DismissKeyboard>
