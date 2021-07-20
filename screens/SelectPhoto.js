c348f8a2e3ca8cefa7d0f47293a8916555b77db2
inch <sxin2949@naver.com>
2021-07-20 22:51:27 +0900

    #17.3 Select Photo part Two

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
+import * as MediaLibrary from "expo-media-library";
 import styled from "styled-components/native";
 
 const Container = styled.View`
@@ -16,10 +17,37 @@ const Bottom = styled.View`
 `;
 
 export default function SelectPhoto() {
+  const [ok, setOk] = useState(false); 
+  const [photos, setPhotos] = useState([]);  // getPhotos로 가져온 사진을 담을 배열 
+  const getPhotos = async () => {   // 요청이 수락되면 getPhotos를 통해서 사진들을 가져올 수 있음. 
+    if (ok) {
+      const { assets: photos } = await MediaLibrary.getAssetsAsync();
+      setPhotos(photos);
+    }
+  };
+  const getPermissions = async () => {  // 앨범에서 사진 가져오는 거 요청 
+    const {
+      accessPrivileges,
+      canAskAgain,
+    } = await MediaLibrary.getPermissionsAsync();
+    if (accessPrivileges === "none" && canAskAgain) {
+      const { accessPrivileges } = await MediaLibrary.requestPermissionsAsync();
+      if (accessPrivileges !== "none") {
+        setOk(true);
+      }
+    } else if (accessPrivileges !== "none") {
+      setOk(true);
+    }
+  };
+  useEffect(() => {
+    getPermissions();
+    getPhotos();
+  }, []);
+
   return (
     <Container>
       <Top />
-      <Bottom />
+      <Bottom></Bottom>
     </Container>
   );
 }
