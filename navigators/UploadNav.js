04ac9cfa88f714995308c9b9c78c73b8728fcef8
inch <sxin2949@naver.com>
2021-07-20 22:34:38 +0900

    #17.2 Select Photo part One

--- navigators/UploadNav.js
+++ navigators/UploadNav.js
@@ -3,7 +3,7 @@ import { createMaterialTopTabNavigator } from "@react-navigation/material-top-ta
 import SelectPhoto from "../screens/SelectPhoto";
 import TakePhoto from "../screens/TakePhoto";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import {Ionicons} from "@expo/vector-icons";
 const Tab = createMaterialTopTabNavigator();
 const Stack = createStackNavigator();
 
@@ -24,8 +24,16 @@ export default function UploadNav() {
     >
       <Tab.Screen name="Select">
         {() => (
-          <Stack.Navigator>
-            <Stack.Screen name="Select" component={SelectPhoto} />
+          <Stack.Navigator screenOptions={{
+            headerTintColor:"white",
+            headerBackTitleVisible: false,
+            headerBackImage: ({tintColor}) => <Ionicons color={tintColor} name="close" size={28} />,
+            headerStyle: {
+              backgroundColor: "black",
+              shadowOpacity: 0.5,
+            }
+          }}>
+            <Stack.Screen name="Select" options={{title: "사진 선택"}} component={SelectPhoto} />
           </Stack.Navigator>
         )}
       </Tab.Screen>
--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Text, View } from "react-native";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+  flex:1;
+  background-color: black;
+`;
+const Top = styled.View`
+  flex:1;
+  background-color:blue;
+`;
+const Bottom = styled.View`
+  flex:1;
+  background-color:red;
+`;
 
 export default function SelectPhoto() {
   return (
-    <View>
-      <Text>SelectPhoto</Text>
-    </View>
+    <Container>
+      <Top />
+      <Bottom />
+    </Container>
   );
 }
